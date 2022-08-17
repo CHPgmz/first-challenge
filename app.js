03ff503bb73dee97457e7c20e3b9bdaee353a802
Alfredo <j4_txmjr@gmx.com>
2022-08-17 14:46:06 -0500

    Se corrigieron algunos errores

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const form = document.getElementById("form");
 const btnEncriptar = document.querySelector("#encriptar");
 const btnDecrifrar = document.querySelector("#decifrar");
+const buttonCP = document.getElementById("button");
+const elmCnt = document.getElementById("elm-cnt");
 
+window.addEventListener("load", () => {
+  if(screen.width < 750){
+    elmCnt.classList.add('element-container-disabled');
+  }
+  
+});
 //para cifrar el texto
-btnEncriptar.addEventListener("submit", getText);
-function getText(e) {
+form.addEventListener("submit", setText);
+function setText(e) {
   const texto = document.getElementById("texto").value;
-  setCript(texto);
+  setCript(texto.toLowerCase());
+  //  setDecrypt(texto);
   form.reset();
   e.preventDefault();
 }
-//Para decifrar el texto
-btnDecrifrar.addEventListener("submit", decrypt);
-function decrypt(e) {
+
+btnDecrifrar.addEventListener("click", (e) => {
   const texto = document.getElementById("texto").value;
-  setDecrypt(texto);
+
+  if (texto.length === 0) {
+    alert("Ingresaste valores nulos o valores invalidos. \nRevisa y corrija el formulario");
+  } else {
+    setDecrypt(texto.toLowerCase());
+  }
   form.reset();
-  e.preventDefault;
-}
+  e.preventDefault();
+});
 
 const claves = {
   e: "enter",
@@ -29,6 +42,18 @@ const claves = {
 
 function setCript(texto) {
   const txtCifrado = document.getElementById("texto-cifrado");
+  const button = document.getElementById("button");
+  if(screen.width < 750){
+  elmCnt.classList.remove("element-container-disabled");
+  elmCnt.classList.add("element-container");
+  button.classList.remove("disabled");
+  button.classList.add("active");
+
+  }else if(screen.width >= 750){  
+  button.classList.remove("disabled");
+  button.classList.add("active");
+  }
+  txtCifrado.classList.add("cifrado");
 
   let frase = " ";
   for (const i in claves) {
@@ -41,11 +66,39 @@ function setCript(texto) {
 
 function setDecrypt(texto) {
   const txtCifrado = document.getElementById("texto-cifrado");
+  const button = document.getElementById("button");
+  //console.log(txtCifrado);
   let frase = " ";
+  if(screen.width < 750){
+  elmCnt.classList.remove("element-container-disabled");
+  elmCnt.classList.add("element-container");
+  button.classList.remove("disabled");
+  button.classList.add("active");
+
+  }else if(screen.width >= 750){  
+  button.classList.remove("disabled");
+  button.classList.add("active");
+  }
+
+  txtCifrado.classList.add("cifrado");
+
   for (const i in claves) {
     frase = texto.replaceAll(claves[i], i);
     texto = frase;
   }
-
   txtCifrado.textContent = frase;
 }
+
+buttonCP.addEventListener("click", (e) => {
+  let txtCopied = document.querySelector("#texto-cifrado");
+  let msj = document.querySelector("#msj");
+  msj.classList.remove("msj-disabled");
+  msj.classList.add("msj-active");
+  let copied = txtCopied.innerHTML;
+  navigator.clipboard.writeText(copied);
+
+  setTimeout(() => {
+    msj.classList.remove("msj-active");
+  }, 3000);
+  //console.log(copied);
+});
